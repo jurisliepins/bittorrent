65121761a7e500ffbc87a78d018047069af47f8c
Juris Liepins <juris.liepins@hotmail.com>
2024-07-24 18:42:03 +0000

    Minor rename

--- bittorrent/bittorrent-api/src/main/java/com/github/jurisliepins/client/ClientProducer.java
+++ bittorrent/bittorrent-api/src/main/java/com/github/jurisliepins/client/ClientProducer.java
@@ -1,11 +1,11 @@
-package com.github.jurisliepins.core;
+package com.github.jurisliepins.client;
 
 import com.github.jurisliepins.BitTorrentClient;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Singleton;
 
 @Singleton
-public final class Producers {
+public final class ClientProducer {
 
     @Produces
     public BitTorrentClient bitTorrentClient() {
