083817767fb4cde0915e0f520b578d17fd04a348
Juris Liepins <juris.liepins@hotmail.com>
2024-09-08 17:10:40 +0100

    Import

--- bittorrent-core/src/test/java/com/github/jurisliepins/bitfield/BitfieldTests.java
+++ bittorrent-core/src/test/java/com/github/jurisliepins/bitfield/BitfieldTests.java
@@ -3,7 +3,6 @@ package com.github.jurisliepins.bitfield;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
