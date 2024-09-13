550ba0f1a140968503e7bc4abd8840644cfce41f
Juris Liepins <juris.liepins@hotmail.com>
2024-09-13 11:56:02 +0100

    Minor

--- bittorrent-core/src/main/java/com/github/jurisliepins/info/InfoHash.java
+++ bittorrent-core/src/main/java/com/github/jurisliepins/info/InfoHash.java
@@ -2,62 +2,58 @@ package com.github.jurisliepins.info;
 
 import lombok.NonNull;
 
-import java.util.Arrays;
 import java.util.HexFormat;
 
 public final class InfoHash {
-    private static final int BYTES_LENGTH = 20;
-    private static final int STRING_LENGTH = 40;
-
     public static final InfoHash BLANK = new InfoHash();
 
-    private final byte[] bytes;
-    private final String string;
-    private final int hashCode;
+    private final String value;
 
     public InfoHash(final byte @NonNull [] bytes) {
-        if (bytes.length != BYTES_LENGTH) {
-            throw new IllegalArgumentException("Bytes length must be %s characters".formatted(BYTES_LENGTH));
-        }
-        this.bytes = bytes;
-        this.string = HexFormat.of().formatHex(bytes);
-        this.hashCode = Arrays.hashCode(bytes);
+        this.value = HexFormat.of().formatHex(requireValid(bytes));
     }
 
     public InfoHash(@NonNull final String string) {
-        if (string.length() != STRING_LENGTH) {
-            throw new IllegalArgumentException("String length must be %s characters".formatted(STRING_LENGTH));
-        }
-        this.string = string;
-        this.bytes = HexFormat.of().parseHex(string);
-        this.hashCode = Arrays.hashCode(bytes);
+        this.value = requireValid(string);
     }
 
     private InfoHash() {
-        this.bytes = new byte[]{};
-        this.string = "";
-        this.hashCode = Arrays.hashCode(bytes);
+        this.value = "";
     }
 
     public byte[] toByteArray() {
-        return Arrays.copyOf(bytes, bytes.length);
+        return HexFormat.of().parseHex(value);
     }
 
     @Override
     public String toString() {
-        return string;
+        return value;
     }
 
     @Override
     public int hashCode() {
-        return hashCode;
+        return value.hashCode();
     }
 
     @Override
     public boolean equals(final Object other) {
         return switch (other) {
-            case InfoHash hash -> Arrays.equals(bytes, hash.bytes);
+            case InfoHash that -> value.equals(that.value);
             default -> false;
         };
+    }
+
+    private static byte[] requireValid(final byte @NonNull [] bytes) {
+        if (bytes.length != 20) {
+            throw new IllegalArgumentException("Bytes length must be 20 characters");
+        }
+        return bytes;
+    }
+
+    private static String requireValid(@NonNull final String string) {
+        if (string.length() != 40) {
+            throw new IllegalArgumentException("String length must be 40 characters");
+        }
+        return string;
     }
 }
--- bittorrent-core/src/main/java/com/github/jurisliepins/peer/PeerId.java
+++ bittorrent-core/src/main/java/com/github/jurisliepins/peer/PeerId.java
@@ -3,66 +3,62 @@ package com.github.jurisliepins.peer;
 import lombok.NonNull;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Random;
 
 import static com.github.jurisliepins.context.Context.CLIENT_ID;
 import static com.github.jurisliepins.context.Context.CLIENT_VERSION;
 
 public final class PeerId {
-    private static final int BYTES_LENGTH = 20;
-    private static final int STRING_LENGTH = 20;
-
     public static final PeerId BLANK = new PeerId();
 
-    private final byte[] bytes;
-    private final String string;
-    private final int hashCode;
+    private final String value;
 
     public PeerId(final byte @NonNull [] bytes) {
-        if (bytes.length != BYTES_LENGTH) {
-            throw new IllegalArgumentException("Bytes length must be %s characters".formatted(BYTES_LENGTH));
-        }
-        this.bytes = bytes;
-        this.string = new String(bytes, StandardCharsets.US_ASCII);
-        this.hashCode = Arrays.hashCode(bytes);
+        this.value = new String(requireValid(bytes), StandardCharsets.US_ASCII);
     }
 
     public PeerId(@NonNull final String string) {
-        if (string.length() != STRING_LENGTH) {
-            throw new IllegalArgumentException("String length must be %s characters".formatted(STRING_LENGTH));
-        }
-        this.string = string;
-        this.bytes = string.getBytes(StandardCharsets.US_ASCII);
-        this.hashCode = Arrays.hashCode(bytes);
+        this.value = requireValid(string);
     }
 
     private PeerId() {
-        this.string = "";
-        this.bytes = new byte[]{};
-        this.hashCode = Arrays.hashCode(bytes);
+        this.value = "";
     }
 
     public byte[] toByteArray() {
-        return Arrays.copyOf(bytes, bytes.length);
+        return value.getBytes(StandardCharsets.US_ASCII);
     }
 
     @Override
     public String toString() {
-        return string;
+        return value;
     }
 
     @Override
     public int hashCode() {
-        return hashCode;
+        return value.hashCode();
     }
 
     @Override
     public boolean equals(final Object other) {
         return switch (other) {
-            case PeerId hash -> Arrays.equals(bytes, hash.bytes);
+            case PeerId that -> value.equals(that.value);
             default -> false;
         };
+    }
+
+    private static byte[] requireValid(final byte @NonNull [] bytes) {
+        if (bytes.length != 20) {
+            throw new IllegalArgumentException("Bytes length must be 20 characters");
+        }
+        return bytes;
+    }
+
+    private static String requireValid(@NonNull final String string) {
+        if (string.length() != 20) {
+            throw new IllegalArgumentException("String length must be 20 characters");
+        }
+        return string;
     }
 
     public static PeerId self() {
